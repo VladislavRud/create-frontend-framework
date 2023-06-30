cca905b44c7dbaaef79e9612bdc2d5294659dbeb
Vladislav <v.rudnitsky29@gmail.com>
2023-06-30 12:34:47 +0300

    templating

--- framework/element.js
+++ framework/element.js
@@ -1,1 +1,10 @@
-const hi = 'hi'
+const createElement = tagName => (strings, ...args) => ({
+    type: tagName,
+    template: strings.reduce(
+        (acc, currentString, index) => acc + currentString + (args[index] || ""),
+        ""
+    )
+});
+
+export const div = createElement("div");
+export const p = createElement("p");
